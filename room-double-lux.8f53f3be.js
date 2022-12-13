e17094241ed95c3b173e8cc14d7e1bedd3f8c257
BoikoIgor <111757412+BoikoIgor@users.noreply.github.com>
2022-12-13 17:13:13 +0000

    Deploying to gh-pages from @ BoikoIgor/team-project-one@3f0462a7156bad708aa6faeca634f9a8114b449f 🚀

---
+++ room-double-lux.8f53f3be.js
@@ -0,0 +1,2 @@
+$(".fade").slick({dots:!1,infinite:!0,speed:500,fade:!0,cssEase:"250ms cubic-bezier(0.4, 0, 0.2, 1)",autoplay:!1,arrows:!0,adaptiveHeight:!0}),$(".fade2").slick({dots:!0,infinite:!0,speed:500,slidesToShow:2,fade:!0,cssEase:"linear",autoplay:!0});
+//# sourceMappingURL=room-double-lux.8f53f3be.js.map
