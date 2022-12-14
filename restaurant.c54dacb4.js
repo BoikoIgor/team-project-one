2f3ddc5dfd38526c4f6b2b566ca52498601ffac6
BoikoIgor <111757412+BoikoIgor@users.noreply.github.com>
2022-12-14 07:28:32 +0000

    Deploying to gh-pages from @ BoikoIgor/team-project-one@cd0421c01ffbbec47ececc574ca69182d3401799 🚀

--- restaurant.c54dacb4.js
+++
@@ -1,2 +0,0 @@
-$(".fade").slick({dots:!1,infinite:!0,speed:500,fade:!0,cssEase:"250ms cubic-bezier(0.4, 0, 0.2, 1)",autoplay:!1,arrows:!0,adaptiveHeight:!0}),$(".fade2").slick({dots:!1,infinite:!0,speed:500,slidesToShow:2,fade:!0,cssEase:"linear",autoplay:!0});
-//# sourceMappingURL=restaurant.c54dacb4.js.map
