3a5a5ab99bddee473dce4dcb21f01e73b20ab5ef
BoikoIgor <111757412+BoikoIgor@users.noreply.github.com>
2022-12-14 08:03:49 +0000

    Deploying to gh-pages from @ BoikoIgor/team-project-one@f2f8c461d6446d4732dfa1ce331de9cf9e02db8b 🚀

--- restaurant.460cf659.js
+++
@@ -1,2 +0,0 @@
-!function(){function e(e){return e&&e.__esModule?e.default:e}var a={};Object.defineProperty(a,"__esModule",{value:!0}),a.default=function(e,a,i){a in e?Object.defineProperty(e,a,{value:i,enumerable:!0,configurable:!0,writable:!0}):e[a]=i;return e},$(".fade").slick({dots:!1,infinite:!0,speed:500,fade:!0,cssEase:"250ms cubic-bezier(0.4, 0, 0.2, 1)",autoplay:!0,arrows:!0,adaptiveHeight:!0}),$(".fade2").slick(e(a)({adaptiveHeight:!0,infinite:!0,speed:500,slidesToShow:2,fade:!0,cssEase:"linear",autoplay:!0},"adaptiveHeight",!0))}();
-//# sourceMappingURL=restaurant.460cf659.js.map
---
+++ restaurant.bbd98834.js
@@ -0,0 +1,2 @@
+$(".fade").slick({dots:!1,infinite:!0,speed:500,fade:!0,cssEase:"250ms cubic-bezier(0.4, 0, 0.2, 1)",autoplay:!0,arrows:!0,adaptiveHeight:!0}),$(".fade2").slick({adaptiveHeight:!0,infinite:!0,speed:500,slidesToShow:2,cssEase:"linear",autoplay:!0,adaptiveHeight:!0,waitForAnimate:!1,variableWidth:!0});
+//# sourceMappingURL=restaurant.bbd98834.js.map
