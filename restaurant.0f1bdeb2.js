1d3289f849e284cc0706a90d1eb4cc3d065c02be
BoikoIgor <111757412+BoikoIgor@users.noreply.github.com>
2022-12-16 15:52:09 +0000

    Deploying to gh-pages from @ BoikoIgor/team-project-one@cd1b6abb4a033dd2c9c06877be7ef9078ca966f7 🚀

---
+++ restaurant.0f1bdeb2.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-room-open]"),closeModalBtn:document.querySelector("[data-room-close]"),modal:document.querySelector("[data-room]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=restaurant.0f1bdeb2.js.map
---
+++ restaurant.e06410d1.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-table-open]"),closeModalBtn:document.querySelector("[data-table-close-table]"),modal:document.querySelector("[data-table]")};function t(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",t),e.closeModalBtn.addEventListener("click",t)}();
+//# sourceMappingURL=restaurant.e06410d1.js.map
